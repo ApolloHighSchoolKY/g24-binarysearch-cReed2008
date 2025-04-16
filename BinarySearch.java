7571b441e2a84f49521180a754d756b2798aa8ca
cReed2008 <colby.reed@stu.daviess.kyschools.us>
2025-04-16 20:40:26 +0000

    Done

--- BinarySearch.java
+++ BinarySearch.java
@@ -4,6 +4,30 @@ public class BinarySearch
 {
 	public static int binarySearch(int[] ray, int item)
 	{
+		int top = ray.length - 1;
+		int low = 0;
+		Arrays.sort(ray); 
 
+		int pos = -1; 
+		while (top >= low)
+		{
+			int mid = (low + top) / 2;
+
+			if (ray[mid] < item)
+			{
+				low = mid + 1;
+			}
+			else if (ray[mid] > item)
+			{
+				top = mid - 1;
+			}
+			else if (ray[mid] == item)
+			{
+				pos = mid;
+				break; 
+			}
+		}
+
+		return pos;
 	}
 }
